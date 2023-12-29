59e9393fa820b202b93677525925cbf3d4ee3e67
Joshua Moon <jcmoon@pm.me>
2023-12-29 01:30:40 +0100

    X support.

--- js/feed/x.js
+++ js/feed/x.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name S.N.E.E.D. (X)
+// @name S.N.E.E.D. (𝕏)
 // @version 1.0.0
 // @description Stream Nexus userscript for Rumble chat.
 // @license BSD-3-Clause
@@ -9,6 +9,7 @@
 // @include https://twitter.com/i/broadcasts/*
 // @include https://x.com/i/broadcasts/*
 // @connect *
+// @sandbox JavaScript
 // @grant GM_getValue
 // @grant GM_setValue
 // @grant GM_deleteValue
@@ -25,13 +26,72 @@
 // @run-at document-start
 // ==/UserScript==
 
+//
+// CONTENT-SECURITY-POLICY (CSP) NOTICE
+// X blocks outbound connections via connect-src, including to local servers.
+// You have to run another extension to edit the policy.
+//
+// https://chromewebstore.google.com/detail/content-security-policy-o/lhieoncdgamiiogcllfmboilhgoknmpi?hl=en
+// ["https://twitter\\.com", [["connect-src", "connect-src ws://127.0.0.2:1350"]]]
+//
+
 (function () {
     'use strict';
+    const PLATFORM = "X";
 
     //
-    // Socket Logic
+    // Monkeypatch WebSocket
     //
-    let CHAT_SOCKET = new WebSocket("ws://localhost:1350/chat.ws");
+    if (WebSocket.prototype.send.sneed_patched === undefined) {
+        console.log("[SNEED] Monkeypatching WebSocket");
+        WebSocket.prototype.oldSendImpl = WebSocket.prototype.send;
+        WebSocket.prototype.send = function (data) {
+            this.oldSendImpl(data);
+            if (this.sneed_patched === undefined) {
+                this.sneed_patched = true;
+                this.addEventListener("message", function (msg) {
+                    if (this.is_sneed_socket !== true) {
+                        const data = JSON.parse(msg.data);
+                        switch (data.kind) {
+                            // chat messages and random junk
+                            case 1:
+                                const payload = JSON.parse(data.payload);
+                                if (payload.sender !== undefined && payload.body !== undefined) {
+                                    const body = JSON.parse(payload.body);
+                                    // Filter updates that do not include text.
+                                    if (body.body !== undefined) {
+                                        console.log(payload.sender, body);
+                                        const messages = HANDLE_MESSAGES([{
+                                            sender: payload.sender,
+                                            body: body
+                                        }]);
+                                        if (messages.length > 0) {
+                                            SEND_MESSAGES(messages);
+                                        }
+                                    }
+                                }
+                                else {
+                                    console.log("[SNEED::X] Unknown message type:", data);
+                                }
+                                break;
+                            // viewer counts
+                            case 2:
+                                // "{"kind":4,"sender":{"user_id":""},"body":"{\"room\":\"1vOxwjQLwPmJB\",\"occupancy\":384,\"total_participants\":19922}"}"
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }, false);
+            }
+        };
+        WebSocket.prototype.send.sneed_patched = true;
+    }
+
+    //
+    // Feed Socket
+    //
+    let CHAT_SOCKET = new WebSocket("ws://127.0.0.2:1350/chat.ws");
     const reconnect = () => {
         // check if socket is connected
         if (CHAT_SOCKET.readyState === WebSocket.OPEN || CHAT_SOCKET.readyState === WebSocket.CONNECTING) {
@@ -39,7 +99,7 @@
         }
         else {
             // attempt to connect if disconnected
-            CHAT_SOCKET = new WebSocket("ws://localhost:1350/chat.ws");
+            CHAT_SOCKET = new WebSocket("ws://127.0.0.2:1350/chat.ws");
         }
     };
 
@@ -58,7 +118,7 @@
     CHAT_SOCKET.addEventListener("error", (event) => {
         console.log("[SNEED::X] Socket has errored. Closing.", event.reason);
         alert("The SNEED chat socket could not connect. Ensure the web server is running and that Brave shields are off.");
-        socket.close();
+        CHAT_SOCKET.close();
     });
 
     //
@@ -69,7 +129,7 @@
     const CREATE_MESSAGE = () => {
         return {
             id: crypto.randomUUID(),
-            platform: "IDK",
+            platform: PLATFORM,
             username: "DUMMY_USER",
             message: "",
             sent_at: Date.now(), // System timestamp for display ordering.
@@ -85,46 +145,32 @@
         };
     };
 
-    const BIND_MUTATION_OBSERVER = () => {
-        const targetNode = GET_CHAT_CONTAINER();
+    const HANDLE_MESSAGES = (pairs) => {
+        var messages = [];
 
-        if (targetNode === null) {
-            return false;
-        }
+        pairs.forEach((pair) => {
+            var message = CREATE_MESSAGE();
 
-        if (document.querySelector(".sneed-chat-container") !== null) {
-            console.log("[SNEED::X] Chat container already bound, aborting.");
-            return false;
-        }
+            message.id = pair.body.uuid;
+            message.username = pair.body.username;
+            message.message = pair.body.body;
+            message.sent_at = pair.body.timestamp;
+            message.avatar = pair.sender.profile_image_url;
+            message.is_verified = pair.sender.verified ?? false;
 
-        targetNode.classList.add("sneed-chat-container");
-
-        const observer = new MutationObserver(MUTATION_OBSERVE);
-        observer.observe(targetNode, {
-            childList: true,
-            attributes: false,
-            subtree: false
+            messages.push(message);
         });
 
-        GET_EXISTING_MESSAGES();
-        return true;
-    };
-
-    const MUTATION_OBSERVE = (mutationList, observer) => {
-        for (const mutation of mutationList) {
-            if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
-                const messages = HANDLE_MESSAGES(mutation.addedNodes);
-                if (messages.length > 0) {
-                    SEND_MESSAGES(messages);
-                }
-            }
-        }
-    };
+        return messages;
+    }
 
     const SEND_MESSAGES = (messages) => {
         // check if socket is open
         if (CHAT_SOCKET.readyState === WebSocket.OPEN) {
-            CHAT_SOCKET.send(JSON.stringify(messages));
+            CHAT_SOCKET.send(JSON.stringify({
+                platform: PLATFORM,
+                messages: messages
+            }));
         }
         else {
             // add to queue if not
@@ -133,58 +179,10 @@
             });
         }
     };
-
-    setInterval(function () {
-        if (document.querySelector(".sneed-chat-container") === null) {
-            console.log("[SNEED::X] Binding chat container.")
-            BIND_MUTATION_OBSERVER();
-        }
-    }, 1000);
-
-
-    //
-    // Specific Implementations
-    //
-
-    const GET_CHAT_CONTAINER = () => {
-        // Thanks Elon
-        return document.querySelector("main div[aria-label='Trending'] > div > div > div:nth-child(2) > div:first-child > div");
-    };
-
-    const GET_EXISTING_MESSAGES = () => {
-        console.log("[SNEED::X] Checking for existing messages.");
-        const nodes = document.querySelectorAll(".sneed-chat-container > div > div");
-
-        if (nodes.length > 0) {
-            const messages = HANDLE_MESSAGES(nodes);
-            if (messages.length > 0)
-                SEND_MESSAGES(messages);
-        }
-    };
-
-    const HANDLE_MESSAGES = (nodes) => {
-        var messages = [];
-
-        nodes.forEach((node) => {
-            var message = CREATE_MESSAGE();
-            node = node.querySelector("div");
-            message.platform = "X";
-
-            message.username = node.querySelector("div:first-child > a > span").innerText.replace(":", "");
-
-            const ch = node.childNodes;
-            const body = document.createElement("div");
-            ch.forEach((n) => {
-                if (n.nodeName.toLowerCase() !== "div")
-                    body.appendChild(n.cloneNode(true));
-            });
-            message.message = body.innerHTML;
-
-            // TODO: broadcaster/owner identification
-
-            messages.push(message);
-        });
-
-        return messages;
-    }
 })();
+
+// wss://prod-chatman-ancillary-us-east-1.pscp.tv/chatapi/v1/chatnow
+// {"kind":2,"payload":"{\"kind\":4,\"sender\":{\"user_id\":\"\"},\"body\":\"{\\\"room\\\":\\\"1BRKjPMNNLwJw\\\",\\\"occupancy\\\":917,\\\"total_participants\\\":22502}\"}"}
+// {"kind":1,"payload":"{\"room\":\"1BRKjPMNNLwJw\",\"body\":\"{\\\"displayName\\\":\\\"Melatonin Milk\\\",\\\"ntpForBroadcasterFrame\\\":16805325508156780544,\\\"ntpForLiveFrame\\\":16805325414151380992,\\\"participant_index\\\":1234993348,\\\"programDateTime\\\":\\\"2023-12-28T23:25:33.008+0000\\\",\\\"remoteID\\\":\\\"1dvKObwBdybQX\\\",\\\"timestamp\\\":1703805954783,\\\"type\\\":2,\\\"uuid\\\":\\\"B56ABCAD-0792-4840-9898-F1D7240601DA\\\",\\\"v\\\":2}\",\"lang\":\"\",\"sender\":{\"user_id\":\"1dvKObwBdybQX\",\"participant_index\":1234993348,\"twitter_id\":\"1445187259595169801\"},\"timestamp\":1703805954891216064}","signature":"3uoKIT2jhEZA6F5tQ_NBFMSU85zGX-rwbzyl7tw"}
+// {"kind":1,"payload":"{\"room\":\"1BRKjPMNNLwJw\",\"body\":\"{\\\"body\\\":\\\"I like trump but I think you are a better version of him\\\",\\\"displayName\\\":\\\"Maurin\\\",\\\"ntpForBroadcasterFrame\\\":16805325789220366336,\\\"ntpForLiveFrame\\\":16805325692963811328,\\\"participant_index\\\":1948053463,\\\"programDateTime\\\":\\\"2023-12-28T23:26:37.952+0000\\\",\\\"remoteID\\\":\\\"12740510\\\",\\\"timestamp\\\":1703806020307,\\\"type\\\":1,\\\"username\\\":\\\"maurinurrutia\\\",\\\"uuid\\\":\\\"C7937812-C85A-43E2-8803-D9A2B163E67C\\\",\\\"v\\\":2}\",\"lang\":\"en-us\",\"sender\":{\"user_id\":\"12740510\",\"username\":\"maurinurrutia\",\"display_name\":\"Maurin\",\"profile_image_url\":\"https://pbs.twimg.com/profile_images/1590517650035195907/II3aAR4K_reasonably_small.jpg\",\"participant_index\":1948053463,\"locale\":\"en\",\"verified\":true,\"twitter_id\":\"277934186\",\"lang\":[\"en\"]},\"timestamp\":1703806020409601023,\"uuid\":\"C7937812-C85A-43E2-8803-D9A2B163E67C\"}","signature":"3ddi0FqY0fshHTkqAqzaoXmRvM4jTBne9cT1xww"}
+// https://prod-chatman-ancillary-us-west-2.pscp.tv/chatapi/v1/history
